c7e5ec1338a06d01ffbc2fc684b0550ad2f63920
aceino <marcelinokamasi76@gmail.com>
2024-03-28 15:55:51 +0700

    layout change

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,34 @@
 function App() {
   return (
     <div className="flex justify-center items-start mx-auto lg:p-10 h-screen">
-      
-      <div className="flex flex-col justify-center items-center lg:w-1/5 bg-red-400 rounded-tl-lg rounded-bl-lg min-h-full p-5">
-      
-        <div className="w-1/2 bg-red-500">
+
+      <div className="flex flex-col justify-start items-center lg:w-1/5 bg-red-400 rounded-tl-lg rounded-bl-lg min-h-full p-5">
+     
+        <div className="flex flex-col justify-center w-full h-60">
+
           <div className="w-20 mx-auto lg:w-28">
             <img src="/image/logo.jpg" alt="logo" />
           </div>
+
           <div className="mt-10">
             <h1 className="font-bold text-lg">Admin</h1>
             <h1 className="font-base text-md">Marcell</h1>
           </div>
+
         </div>
 
-        <div className="w-1/2 block text-center bg-red-700 gap-x-50">
-          <h1>Items</h1>
-          <h1>Item In</h1>
-          <h1>Item Out</h1>
-          <h1>Suppliers</h1>
+        <div className="flex flex-col justify-center  w-full h-72">
+          <h1 className="text-left font-semibold text-lg">Items</h1>
+          <h1 className="text-left font-semibold text-lg">Item In</h1>
+          <h1 className="text-left font-semibold text-lg">Item Out</h1>
+          <h1 className="text-left font-semibold text-lg">Suppliers</h1>
         </div>
+
       </div>
 
       <div className="flex justify-center items-center w-full lg:w-3/4 rounded-tr-lg rounded-br-lg bg-red-200 min-h-full">
         ini datanya
       </div>
-
     </div>
   );
 }
